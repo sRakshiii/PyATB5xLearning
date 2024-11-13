be6484696cc555d887b7afd446ad9871de26bb07
sRakshiii <86668172+sRakshiii@users.noreply.github.com>
2024-11-13 22:34:01 +0530

    12_nov

---
+++ src/ex_12112024_Op/Task_1.py
@@ -0,0 +1,9 @@
+# Write a program to take a user age and let him know if he can go the club.  21
+
+user_age = int(input("enter user age"))
+
+if user_age > 21 :
+    print("User can go to the club")
+else :
+    print("User can not go the club")
+
